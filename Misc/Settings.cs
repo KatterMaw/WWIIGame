dee3d5f77757a9d8477b5d4dc8bd8d37e7ba56ec
KatterMaw <TheKatterDev@gmail.com>
2022-09-01 17:12:56 +0500

    Incapsulate settings to singleton wrapper

---
+++ Misc/Settings.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+namespace WWIIGame.Misc;
+
+public static class Settings
+{
+	private const string Physics3DDefaultGravity = "physics/3d/default_gravity";
+
+	public static float GravityMagnitude
+	{
+		get => (float) ProjectSettings.GetSetting(Physics3DDefaultGravity);
+		set => ProjectSettings.SetSetting(Physics3DDefaultGravity, value);
+	}
+}
