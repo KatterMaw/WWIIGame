3ff4d2ee5dd051d71b2bebc057bf70e2b62ace1a
KatterMaw <TheKatterDev@gmail.com>
2022-09-01 17:13:23 +0500

    Implement StopwatchSingleton.cs to use it sometimes to benchmark; Also install MoreLinq for use SkipLast there;

---
+++ Misc/StopwatchSingleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using MoreLinq;
+
+namespace WWIIGame.Misc;
+
+public static class StopwatchSingleton
+{
+	private static readonly HashSet<double> Records = new();
+	private static readonly Stopwatch Stopwatch = new();
+
+	public static void AddNewRecord() => Records.Add(Stopwatch.Elapsed.TotalMilliseconds);
+
+	public static void Start() => Stopwatch.Restart();
+
+	public static double Median => Records.OrderBy(number => number).Skip(Records.Count / 5).SkipLast(Records.Count / 5).Average();
+}
